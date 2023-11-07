2df90c22e6e951b76dc765440fd42f6266ede555
Daniil Halushka <popolzen22@gmail.com>
2023-11-08 01:18:44 +0300

    Add my agents

---
+++ filter_agent.py
@@ -0,0 +1,54 @@
+import asyncio
+from neo4j import AsyncGraphDatabase
+
+
+class PictureFilterAgent:
+    def __init__(self, uri, username, password):
+        self.driver = AsyncGraphDatabase.driver(uri, auth=(username, password))
+
+    async def execute_query(self, query, parameters=None):
+        async with self.driver.session() as session:
+            result = await session.run(query, parameters=parameters)
+            return result
+
+    async def filter_pictures_by_author_or_name(self, author=None, name=None):
+        if author and name:
+            query = "MATCH (authorNode: Node {name: $author})-[:WROTE]->(pictureNode: Node {name: $name}) RETURN pictureNode"
+            parameters = {"author": author, "name": name}
+        elif author:
+            query = "MATCH (authorNode: Node {name: $author})-[:WROTE]->(pictureNode: Node) RETURN pictureNode"
+            parameters = {"author": author}
+        elif name:
+            query = "MATCH (pictureNode: Node {name: $name}) RETURN pictureNode"
+            parameters = {"name": name}
+        else:
+            return []
+
+        result = await self.execute_query(query, parameters)
+        return [record["pictureNode"]["name"] for record in result]
+
+    async def filter_pictures_by_context(self, request_name, context_author=None, context_name=None):
+        if context_author or context_name:
+            filtered_names = await self.filter_pictures_by_author_or_name(context_author, context_name)
+            query = (
+                "MATCH (requestNode: Class {name: $request_name}), (pictureNode: Node) "
+                "WHERE pictureNode.name IN $filtered_names "
+                "CREATE (requestNode)-[:FOUND_PICTURE]->(pictureNode)"
+            )
+            parameters = {"request_name": request_name, "filtered_names": filtered_names}
+            await self.execute_query(query, parameters)
+
+
+async def main():
+    uri = "bolt://localhost:7687"
+    username = "DANIIL"
+    password = "12345678"
+
+    filter_agent = PictureFilterAgent(uri, username, password)
+
+    await filter_agent.filter_pictures_by_context("concept_request", context_author="Leonardo da Vinci",
+                                                  context_name="Mona Lisa")
+
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(main())
---
+++ searching_agent.py
@@ -0,0 +1,59 @@
+import asyncio
+
+from neo4j import AsyncGraphDatabase
+
+
+class PictureSearchAgent:
+    def __init__(self, uri, username, password):
+        self.driver = AsyncGraphDatabase.driver(uri, auth=(username, password))
+
+    async def execute_query(self, query, parameters=None):
+        async with self.driver.session() as session:
+            result = await session.run(query, parameters=parameters)
+            return result
+
+    async def find_pictures_by_author(self, author):
+        query = "MATCH (authorNode: Node {name: $author})-[:WROTE]->(pictureNode: Node) RETURN pictureNode"
+        result = await self.execute_query(query, parameters={"author": author})
+        return [record["pictureNode"]["name"] for record in result]
+
+    async def find_pictures_by_year(self, year):
+        query = "MATCH (pictureNode: Node {year: $year}) RETURN pictureNode"
+        result = await self.execute_query(query, parameters={"year": year})
+        return [record["pictureNode"]["name"] for record in result]
+
+    async def find_pictures_by_author_and_year(self, author, year):
+        query = "MATCH (authorNode: Node {name: $author})-[:WROTE]->(pictureNode: Node {year: $year}) RETURN pictureNode"
+        result = await self.execute_query(query, parameters={"author": author, "year": year})
+        return [record["pictureNode"]["name"] for record in result]
+
+    async def add_pictures_to_request_context(self, author=None, year=None):
+        if author and not year:
+            names = await self.find_pictures_by_author(author)
+        elif year and author:
+            names = await self.find_pictures_by_author_and_year(author, year)
+        else:
+            names = await self.find_pictures_by_year(year)
+
+        async with self.driver.session() as session:
+            request_node_query = "CREATE (requestNode: Class {name: 'concept_request'})"
+            await session.write_transaction(lambda tx: tx.run(request_node_query))
+
+            for name in names:
+                request_relationship_query = (
+                    "MATCH (pictureNode: Node {name: $name}), (requestNode: Class {name: 'concept_request'}) "
+                    "CREATE (requestNode)-[:FOUND_PICTURE]->(pictureNode)"
+                )
+                await session.write_transaction(lambda tx: tx.run(request_relationship_query, name=name))
+
+
+async def main():
+    uri = "bolt://localhost:7687"
+    username = "DANIIL"
+    password = "12345678"
+    agent = PictureSearchAgent(uri, username, password)
+    await agent.add_pictures_to_request_context(author="Leonardo da Vinci", year="1503")
+
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(main())
