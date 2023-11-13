6c358b70902d08a7d7ebe6a8bebc8e97c18ea369
Daniil Halushka <popolzen22@gmail.com>
2023-11-13 22:28:15 +0300

    Fix searching_agent

--- searching_agent.py
+++ searching_agent.py
@@ -13,7 +13,7 @@ class PictureSearchAgent:
             return result
 
     async def find_pictures_by_author(self, author):
-        query = "MATCH (authorNode: Node {name: $author})-[:WROTE]->(pictureNode: Node) RETURN pictureNode"
+        query = "MATCH (authorNode: Node {name: $author})-[:nrel_write]->(pictureNode: Node) RETURN pictureNode"
         result = await self.execute_query(query, parameters={"author": author})
         return [record["pictureNode"]["name"] for record in result]
 
@@ -23,7 +23,7 @@ class PictureSearchAgent:
         return [record["pictureNode"]["name"] for record in result]
 
     async def find_pictures_by_author_and_year(self, author, year):
-        query = "MATCH (authorNode: Node {name: $author})-[:WROTE]->(pictureNode: Node {year: $year}) RETURN pictureNode"
+        query = "MATCH (authorNode: Node {name: $author})-[:nrel_write]->(pictureNode: Node {year: $year}) RETURN pictureNode"
         result = await self.execute_query(query, parameters={"author": author, "year": year})
         return [record["pictureNode"]["name"] for record in result]
 
@@ -42,7 +42,7 @@ class PictureSearchAgent:
             for name in names:
                 request_relationship_query = (
                     "MATCH (pictureNode: Node {name: $name}), (requestNode: Class {name: 'concept_request'}) "
-                    "CREATE (requestNode)-[:FOUND_PICTURE]->(pictureNode)"
+                    "CREATE (requestNode)-[:nrel_found_picture]->(pictureNode)"
                 )
                 await session.write_transaction(lambda tx: tx.run(request_relationship_query, name=name))
 
